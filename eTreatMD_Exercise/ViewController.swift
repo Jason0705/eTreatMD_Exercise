883f8dd5c698c0ee492da87258bae081b4614c92
Jason Li <jason18424@gmail.com>
2019-09-18 10:09:13 -0700

    Initial Commit

---
+++ eTreatMD_Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eTreatMD_Exercise
+//
+//  Created by Jason Li on 2019-09-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
