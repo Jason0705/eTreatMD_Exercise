2e675f1637248fa51375ead283175137dfbc63c0
Jason Li <jason18424@gmail.com>
2019-09-18 12:56:13 -0700

    Setting up storyboard structure.

---
+++ Pods/Target Support Files/Pods-eTreatMD_Exercise/Pods-eTreatMD_Exercise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eTreatMD_Exercise : NSObject
+@end
+@implementation PodsDummy_Pods_eTreatMD_Exercise
+@end
---
+++ eTreatMD_Exercise/Controllers/Treatment/TreatmentsVC.swift
@@ -0,0 +1,29 @@
+//
+//  TreatmentVC.swift
+//  eTreatMD_Exercise
+//
+//  Created by Jason Li on 2019-09-18.
+//
+
+import UIKit
+
+class TreatmentsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
